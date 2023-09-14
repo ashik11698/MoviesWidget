1bd7d568cb5b570fe871e614a59c99d9a246811f
ashik11698 <mdashikur.rahman@bjitgroup.com>
2023-09-14 09:22:59 +0600

    Initial Commit

---
+++ MoviesWidget/MoviesWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesWidgetApp.swift
+//  MoviesWidget
+//
+//  Created by Mac on 14/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
